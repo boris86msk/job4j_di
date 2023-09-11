0166e72d284791794f3b855a112372561f33096e
boris86msk <boris86msk@yandex.ru>
2023-09-11 12:01:21 +0300

    change path in main()

--- src/main/java/ru/job4j/di/Main.java
+++ src/main/java/ru/job4j/di/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("lifecycle");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.job4j.di.lifecycle");
         context.close();
     }
 }
