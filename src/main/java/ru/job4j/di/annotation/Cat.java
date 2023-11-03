23791590ecd11f61c34422b992dc8ce8904e38e8
Boris <boris86msk@yandex.ru>
2023-11-03 23:24:23 +0300

    Конфигурация контейнера

---
+++ src/main/java/ru/job4j/di/annotation/Cat.java
@@ -0,0 +1,12 @@
+package ru.job4j.di.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat implements Pet<Cat> {
+    private String name = "Cat";
+    @Override
+    public String print() {
+        return "Cat name = %s".formatted(name);
+    }
+}
